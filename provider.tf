a2e9cc11c704aba202b80f136cb50687111107cc
iroshanav <knightshift.kurosaki@gmail.com>
2024-08-23 01:14:06 +0530

    Add | Multiple providers

--- provider.tf
+++ provider.tf
@@ -1,8 +1,24 @@
 # author    https://github.com/iroshanvidanage
 # date      08/20/2024
 
-#configuration objects
+# configuration objects
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
 
+    # azurerm = {
+    #   source = "hashicorp/azurerm"
+    #   version = "~> 4.5"
+    # }
+  }
+}
+
+
+
+# configure AWS provider
 provider "aws" {
   region = var.region
 }
