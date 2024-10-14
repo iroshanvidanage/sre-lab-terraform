d3ba6220f3d3a1535ceff057c283b9f00ce4a46b
iroshanav <knightshift.kurosaki@gmail.com>
2024-10-15 02:12:40 +0530

    Update | Un commented the backend configs

--- backend.tf
+++ backend.tf
@@ -9,18 +9,18 @@ terraform {
 }
 
 
-# # local backend config (this cannot be set dynamically)
-# terraform {
-#   backend "local" {
-#     path = "relative/path/to/terraform.tfstate"
-#   }
-# }
+# local backend config (this cannot be set dynamically)
+terraform {
+  backend "local" {
+    path = "relative/path/to/terraform.tfstate"
+  }
+}
 
-# # local backend config (used to set backend dynamically)
-# data "terraform_remote_state" "foo" {
-#   backend = "local"
+# local backend config (used to set backend dynamically)
+data "terraform_remote_state" "foo" {
+  backend = "local"
 
-#   config = {
-#     path = "${path.module}/../../${terraform_file_name}.tfstate"
-#   }
-# }
+  config = {
+    path = "${path.module}/../../${terraform_file_name}.tfstate"
+  }
+}
