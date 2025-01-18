46ac32c010f70f16bf497a1e5e27011cde26076e
iroshanav <knightshift.kurosaki@gmail.com>
2025-01-19 00:27:43 +0530

    Update | add tags

--- challenge2/challenge_2.tf
+++ challenge2/challenge_2.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 }
@@ -14,6 +14,18 @@ variable "splunk" {
   default = "8088"
 }
 
+variable "payment_app_cidr" {
+  default = "172.31.0.0/16"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "environment" {
+  default = "DEV"
+}
+
 resource "aws_security_group" "security_group_payment_app" {
   name        = "payment_app"
   description = "Application Security Group"
@@ -24,7 +36,7 @@ resource "aws_security_group" "security_group_payment_app" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["172.31.0.0/16"]
+    cidr_blocks = [var.payment_app_cidr]
   }
 
   # Below ingress allows APIs access from DEV VPC
@@ -33,7 +45,7 @@ resource "aws_security_group" "security_group_payment_app" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["172.31.0.0/16"]
+    cidr_blocks = [var.payment_app_cidr]
   }
 
   # Below ingress allows APIs access from Prod App Public IP.
@@ -51,12 +63,29 @@ resource "aws_security_group" "security_group_payment_app" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = merge(local.common_tags,
+    tomap(
+      { "Category" = "Private" }
+    )
+  )
+
 }
 
 resource "aws_eip" "example" {
   domain = "vpc"
+
+  tags = merge(local.common_tags,
+    tomap(
+      { "Category" = "Public" }
+    )
+  )
 }
 
-variable "region" {
-  default = "us-east-1"
+locals {
+  common_tags = {
+    Environment = var.environment
+    Terraform   = true
+    MangedBy    = "CSRE"
+  }
 }
